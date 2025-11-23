c1dc93c45fe5e61a1ddbf2d91be205c4ca3ac1a2
nguyendangminh-coder <dangminh192102@gmail.com>
2025-11-23 16:36:28 +0700

    Refactor Flywheel class and update motor behavior

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Flywheel.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Flywheel.java
@@ -3,20 +3,22 @@ package org.firstinspires.ftc.teamcode.subsystem;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
-
-public class Flywheel implements Subsystem {
+import com.qualcomm.robotcore.util.Range;
+public class Flywheel {
     private DcMotor motor;
     private Telemetry telemetry;
     private final double VELOCITY_INCREMENT = 0.05;//bien tang giam toc do flywheel
     private double power;
 
-    @Override
     public void init(HardwareMap hwMap, Telemetry telemetry) {
         this.telemetry = telemetry;
         this.motor = hwMap.get(DcMotor.class, "flywheel");
         this.motor.setDirection(DcMotor.Direction.REVERSE);
         this.motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        this.motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
 
+    }
+    public void start() {
         this.power = 0.0;
         this.motor.setPower(this.power);
     }
@@ -27,25 +29,19 @@ public class Flywheel implements Subsystem {
         if (decrease) {
             power -= VELOCITY_INCREMENT;
         }
-        if (power > 1.0) {
-            power = 1.0;
-        }
-        if (power < 0.0) {
-            power = 0.0;
-        }
+        power = Range.clip(power, 0.0, 1.0);
+
         this.motor.setPower(power);
     }
     public double getPower() {
         return this.power;
     }
 
-    @Override
     public void periodic() {
         telemetry.addData("Flywheel Target Power", "%.2f", getPower());
         telemetry.addData("Flywheel Actual Power", "%.2f", this.motor.getPower());
     }
-    
-    @Override
+
     public void stop() {
         this.power = 0.0;
         this.motor.setPower(this.power);
