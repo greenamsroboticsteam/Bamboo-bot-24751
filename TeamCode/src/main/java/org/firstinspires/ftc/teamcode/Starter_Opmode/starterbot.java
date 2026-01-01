2281ac4de6cfde92833f434efc61e378313d2091
nguyendangminh-coder <dangminh192102@gmail.com>
2026-01-01 23:04:16 +0700

    Update starterbot.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Starter_Opmode/starterbot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Starter_Opmode/starterbot.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.Starter_Opmode;
 
+import com.bylazar.configurables.annotations.Configurable;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -7,7 +8,7 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.ServoImplEx;
-import com.qualcomm.robotcore.util.ElapsedTime;
+@Configurable
 @TeleOp(name = "starterbot")
 public class starterbot extends OpMode {
     Gamepad curr1 = null;
@@ -21,39 +22,31 @@ public class starterbot extends OpMode {
     private DcMotorEx flywheel;
 
     private ServoImplEx block;
+    private ServoImplEx block2;
 
     private boolean intakeToggle = false;
     private boolean flywheelToggle = false;
 
-    private ElapsedTime mRuntime;
-
-    private double integralSum = 0;
-    private double lastError = 0;
-    private ElapsedTime pidTimer = new ElapsedTime(ElapsedTime.Resolution.SECONDS);
-
-    private static final double TICKS_PER_REV = 537.7; // Đây là con số cho motor Gobilda 5203 series
-    public static double Kp = 0.1;
-    public static double Ki = 0.0;
-    public static double Kd = 0.0001;
-    public static double Kf = 0.5;
-    public static double TARGET_VELOCITY = 1500;
-
+    private double power = 0.5;
     @Override
-    public void init(){
+    public void init() {
         prev1 = new Gamepad();
         curr1 = new Gamepad();
 
         intake = hardwareMap.get(DcMotorEx.class, "intake");
         intake.setDirection(DcMotorSimple.Direction.REVERSE);
-        intake.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.FLOAT);
+        intake.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
 
         flywheel = hardwareMap.get(DcMotorEx.class, "flywheel");
-        flywheel.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        flywheel.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         flywheel.setDirection(DcMotorSimple.Direction.REVERSE);
         flywheel.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.FLOAT);
 
-        block = hardwareMap.get(ServoImplEx.class, "block");
+        block = hardwareMap.get(ServoImplEx.class, "leftBlock");
         block.setPosition(0);
+
+        block2 = hardwareMap.get(ServoImplEx.class, "rightBlock");
+        block2.setPosition(1.0);
 
         leftMotor = hardwareMap.get(DcMotorEx.class, "leftMotor");
         leftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -62,8 +55,6 @@ public class starterbot extends OpMode {
         rightMotor = hardwareMap.get(DcMotorEx.class, "rightMotor");
         rightMotor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
         rightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
-
-        mRuntime = new ElapsedTime();
     }
 
     @Override
@@ -86,66 +77,43 @@ public class starterbot extends OpMode {
         leftMotor.setPower(leftSideOutput);
         rightMotor.setPower(rightSideOutput);
 
-        if(curr1.left_bumper && !prev1.left_bumper) flywheelToggle = !flywheelToggle;
+        if(curr1.dpad_down && !prev1.dpad_down) {
+            power -= 0.025;
+        }
+        if(curr1.dpad_up && !prev1.dpad_up) {
+            power += 0.025;
+        }
+        if(curr1.left_bumper && !prev1.left_bumper) {
+            flywheelToggle = !flywheelToggle;
+        }
+        if (curr1.right_bumper && !prev1.right_bumper) intakeToggle = !intakeToggle;
 
-        if (curr1.right_bumper && !prev1.right_bumper) intakeToggle = ! intakeToggle;
-
-        intake.setPower((intakeToggle) ? 1 : 0);
-
-        if (curr1.right_trigger > 0.2) {
+        if (curr1.x) {
+            intakeToggle = false;
+            intake.setPower(0.5);
+        } else if (curr1.right_trigger > 0.2) {
+            intakeToggle = false;
             intake.setPower(-1);
-        }
-        else if (curr1.left_trigger < 0.2) {
-            intake.setPower(0);
-        }
+        } else if (intakeToggle) {
+            intake.setPower(0.7);
+        } else intake.setPower(0.0);
 
-        if (curr1.circle && !prev1.circle) {
-            mRuntime.reset();
-        }
-
-        if (flywheelToggle) {
-            double targetTicksPerSecond = (TARGET_VELOCITY * TICKS_PER_REV) / 60.0;
-
-            double power = PIDF(targetTicksPerSecond) * 0.67;
-            flywheel.setPower(power);
+        if(!flywheelToggle) {
+            flywheel.setPower(0.0);
         } else {
-            flywheel.setPower(0);
-            integralSum = 0;
-            lastError = 0;
-            pidTimer.reset();
+            flywheel.setPower(power);
         }
 
-        if (mRuntime.seconds() < 5.0) {
+        if (curr1.x) {
             block.setPosition(0.4);
+            block2.setPosition(0.6);
         } else {
             block.setPosition(0);
+            block2.setPosition(1.0);
         }
-        double currentTicksPerSecond = flywheel.getVelocity();
-        double currentRPM = (currentTicksPerSecond * 60.0) / TICKS_PER_REV;
 
-        telemetry.addData("Target RPM", TARGET_VELOCITY);
-        telemetry.addData("Current TPS", "%.2f", currentRPM); 
+        telemetry.addData("flywheel Position: ", flywheel.getPower());
+        telemetry.addData("Servo Position: ", block.getPosition());
         telemetry.update();
     }
-    public double PIDF(double targetVelocity) {
-        if (pidTimer.time() == 0) pidTimer.reset();
-
-        double currentVelocity = flywheel.getVelocity();
-        double error = targetVelocity - currentVelocity;
-
-        if(pidTimer.time() < 0.00001) {
-            return (Kp * error) + (Kf * targetVelocity);
-        }
-        integralSum = integralSum + (error * pidTimer.time());
-
-        double derivative = (error - lastError) / pidTimer.time();
-
-        lastError = error;
-        pidTimer.reset();
-
-        double outputPower = (Kp * error) + (Ki * integralSum) + (Kd * derivative) + (Kf * targetVelocity);
-
-        return outputPower;
-    }
-
 }
